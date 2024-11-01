12fdedb9f77c519c004148eaf7f2f6dced355d61
belelemllukas <lukasbelemel@gmail.com>
2024-11-01 14:31:52 -0300

    atualizar

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"+wGuynXANhHj/VrENG+oVty+84i2jBYC5W59a97PS/Y=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ZCpketZa6udBq0IgHMVfIEfzbqV9WaCkywMNtmVmFcY=\"\n}"
--- frontend/.next/standalone/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"+wGuynXANhHj/VrENG+oVty+84i2jBYC5W59a97PS/Y=\"}"
