2169e787dde49cda6f5a0a14bb0ff3bd34fb9a0e
belelemllukas <lukasbelemel@gmail.com>
2024-11-02 17:21:20 -0300

    atualizacao

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({
+    origin: 'http://54.144.36.232:3000',
+    credentials: true, 
+  })
   await app.listen(8000);
 }
 bootstrap();
